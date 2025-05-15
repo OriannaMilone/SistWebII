e516e267a7ecd4e6cbcb601c946fbd200eb89302
Orianna Milone <oriannamilone.s@gmail.com>
2025-05-15 15:21:59 +0200

    ejercicio Ajv y Node listo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SistWebII
+
+### Charapter 2: JSON Schema
+
+```bash
+    npm install ajv
+    npm install ajv-formats
+```
+To validare JSON and their schemas: https://www.jsonschemavalidator.net/
--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
+const ajv = new Ajv({strict: false, validateSchema: false});
+
+addFormats(ajv)
+
+const raceSchema = require("./schemas/race_schema.json");
+const rutineSchema = require("./schemas/gym_rutine_schema.json");
+
+app.post('/race', (req, res) => {
+  const validateRace = ajv.compile(raceSchema);
+  const isValid = validateRace(req.body);
+  
+  if (isValid) {
+    res.status(200).send("Race JSON is valid");
+  } else {
+    console.log(validateRace.errors);
+    res.status(400).send("Race JSON is invalid");
+  }
+});
+
+app.post('/rutine', (req, res) => {
+  const validateRutine = ajv.compile(rutineSchema);
+  const isValid = validateRutine(req.body);
+  
+  if (isValid) {
+    res.status(200).send("Rutine JSON is valid");
+  } else {
+    console.log(validateRutine.errors);
+    res.status(400).send("Rutine JSON is invalid");
+  }
+});
+
 
 
 app.listen(3000, () => {
